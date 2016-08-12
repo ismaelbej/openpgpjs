5a3df2b7ee06ad2c56315fa7204c3c081083a963
Ismael Bejarano <ismael.bejarano@coinfabrik.com>
2016-08-12 00:25:36 -0300

    Unit test for elliptic curve cryptography

--- test/general/index.js
+++ test/general/index.js
@@ -1,6 +1,7 @@
 describe('General', function () {
   require('./util.js');
   require('./armor.js');
+  require('./ecc.js');
   require('./packet.js');
   require('./keyring.js');
   require('./signature.js');
